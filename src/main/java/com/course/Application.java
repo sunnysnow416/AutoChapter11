f0dd83037ed49b10e6344cb4d77526605db5d180
sunnysnow416 <570507051@qq.com>
2020-04-26 16:26:03 +0800

    20200426 可成功查询到结果，提交

---
+++ src/main/java/com/course/Application.java
@@ -0,0 +1,25 @@
+package com.course;
+
+import javax.annotation.PreDestroy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+
+//万能入口应用程序
+@EnableScheduling
+@SpringBootApplication
+public class Application {
+	private static ConfigurableApplicationContext context;
+	
+	public static void main(String[] args) {
+		Application.context=SpringApplication.run(Application.class, args);
+	}
+	
+	@PreDestroy
+	public void close(){
+		Application.context.close();
+	}
+}
